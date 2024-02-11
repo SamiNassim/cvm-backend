9dd5c9b737fab3869d49d99b8ad18cd1bc639102
Sami Nassim <sami.bououdine@gmail.com>
2024-02-11 23:08:24 +0100

    First commit, added database configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# CVM - Backend
+
+Réécriture du backend entièrement en Java. (Le backend était initialement inclus dans une app NextJS)
+
+*More coming soon !...*
---
+++ src/test/java/com/saminassim/cvm/CvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saminassim.cvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
